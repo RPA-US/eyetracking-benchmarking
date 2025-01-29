1775da3a89ec6b67265ff9e3f631cc1f2bf29bbf
Manu Garcia <mgrmanu10@gmail.com>
2025-01-29 09:04:05 +0100

    final_results_ouput generated

--- plots.py
+++ plots.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
+import datetime
 
 # Crear la carpeta si no existe
-os.makedirs('tests/figs', exist_ok=True)
+os.makedirs('output/figs', exist_ok=True)
 
 # Especificar la ruta base del proyecto y la salida deseada
 base_path = "data/data_collection"
@@ -38,21 +39,21 @@ percentage_matching_fixation_rq1_webgazer_form_density_high = rq1_webgazer_form_
 
 # Datos actualizados RQ1
 data = {
-    '% Matching Fixation': [percentage_matching_fixation_rq1_tobii_form_density_high,
-                            percentage_matching_fixation_rq1_tobii_form_density_low,
-                            percentage_matching_fixation_rq1_webgazer_form_density_high,
-                            percentage_matching_fixation_rq1_webgazer_form_density_low],
+    '% Matching Fixation': [percentage_matching_fixation_rq1_tobii_form_density_low,
+                            percentage_matching_fixation_rq1_tobii_form_density_high,
+                            percentage_matching_fixation_rq1_webgazer_form_density_low,
+                            percentage_matching_fixation_rq1_webgazer_form_density_high],
     
-    'Tool/Software': ['Infrared/Tobii', 'Infrared/Tobii', 'Webcam/Webgazer.js', 'Webcam/Webgazer.js'],
-    'Scenario': ['Form High Density', 'Form Low Density', 'Form High Density', 'Form Low Density']
+    'Device/Software (TC Scenario)': ['Infrared/Tobii', 'Infrared/Tobii', 'Webcam/Webgazer.js', 'Webcam/Webgazer.js'],
+    'Test Case Scenario': ['TC1', 'TC2', 'TC1', 'TC2']
 }
 
 df = pd.DataFrame(data)
-df['Tool/Software (Scenario)'] = df['Tool/Software'] + ' (' + df['Scenario'] + ')'
+df['Device/Software (TC Scenario)'] = df['Device/Software (TC Scenario)'] + ' (' + df['Test Case Scenario'] + ')'
 
 # Crear la gráfica
 fig, ax = plt.subplots(figsize=(10, 6))
-bars = plt.bar(df['Tool/Software (Scenario)'], df['% Matching Fixation'], 
+bars = plt.bar(df['Device/Software (TC Scenario)'], df['% Matching Fixation'], 
                color=['orange', 'green', 'red', 'blue'])
 
 # Etiquetas de porcentaje en las barras
@@ -61,14 +62,14 @@ for bar, pct in zip(bars, df['% Matching Fixation']):
     plt.text(bar.get_x() + bar.get_width() / 2.0, height, f'{pct:.2f}%', ha='center', va='bottom', fontsize=10)
 
 # Personalización del gráfico
-plt.title('% Matching Fixation by Tool/Software and Scenario', fontsize=14)
-plt.ylabel('% Matching Fixation', fontsize=12)
+plt.title('% Matching Fixation (%MF) by Device/Software and Test Case Scenario', fontsize=12)
+plt.ylabel('% MF', fontsize=12)
 plt.xticks(rotation=45, ha='right', fontsize=10)
 plt.ylim(0, 100)
 plt.tight_layout()
 
 # Guardar la gráfica
-plt.savefig('tests/figs/RQ1_matching_fixations.jpg')
+plt.savefig('output/figs/RQ1_matching_fixations.jpg')
 # Mostrar la gráfica
 # plt.show()
 
@@ -85,14 +86,14 @@ percentage_matching_fixation_rq2_webgazer_alternance_buttons = rq2_webgazer_alte
 new_data_rq2 = {
     '% Matching Fixation': [percentage_matching_fixation_rq2_tobii_alternance_buttons,
                             percentage_matching_fixation_rq2_webgazer_alternance_buttons],
-    'Tool/Software': ['Infrared/Tobii', 'Webcam/Webgazer.js']
+    'Device/Software': ['Infrared/Tobii', 'Webcam/Webgazer.js']
 }
 
 new_df = pd.DataFrame(new_data_rq2)
 
 # Crear la gráfica
 fig, ax = plt.subplots(figsize=(8, 5))
-bars = plt.bar(new_df['Tool/Software'], new_df['% Matching Fixation'], color=['orange', 'skyblue'])
+bars = plt.bar(new_df['Device/Software'], new_df['% Matching Fixation'], color=['orange', 'skyblue'])
 
 # Etiquetas de porcentaje en las barras
 for bar, pct in zip(bars, new_df['% Matching Fixation']):
@@ -100,14 +101,14 @@ for bar, pct in zip(bars, new_df['% Matching Fixation']):
     plt.text(bar.get_x() + bar.get_width() / 2.0, height, f'{pct:.2f}%', ha='center', va='bottom', fontsize=10)
 
 # Personalización del gráfico
-plt.title('% Matching Fixation by Tool/Software', fontsize=14)
-plt.ylabel('% Matching Fixation', fontsize=12)
+plt.title('% Matching Fixations (%MF) by device', fontsize=14)
+plt.ylabel('% MF', fontsize=12)
 plt.xticks(rotation=0, ha='center', fontsize=10)
 plt.ylim(0, 100)
 plt.tight_layout()
 
 # Guardar la gráfica
-plt.savefig('tests/figs/RQ2_Matching_fixation.jpg')
+plt.savefig('output/figs/RQ2_Matching_fixation.jpg')
 # Mostrar la gráfica
 
 
@@ -125,8 +126,8 @@ fig, ax = plt.subplots(figsize=(6, 4))
 bars = ax.bar(tools, percentages, color=['orange','skyblue'])
 
 # Añadir etiquetas y título
-ax.set_ylabel("% Event Including Captured Fixations")
-ax.set_title("Comparison of Tools: %Event Including Captured Fixations")
+ax.set_ylabel("% EIF")
+ax.set_title("% Events Including Fixation (EIF) by device")
 ax.set_ylim(0, 110)  # Limitar el eje Y para espacio adicional
 
 # Mostrar porcentaje en las barras
@@ -137,7 +138,7 @@ for bar in bars:
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ2_events_including_fixations.jpg')
+plt.savefig('output/figs/RQ2_events_including_fixations.jpg')
 # Mostrar gráfico
 # plt.show()
 
@@ -160,7 +161,7 @@ percentage_events_including_fixations_rq3_webgazer_position_50cm = rq3__webgazer
 percentage_events_including_fixations_rq3_webgazer_position_70cm = rq3__webgazer_position_70cm['%EventsWithFixations'].mean()
 percentage_events_including_fixations_rq3_webgazer_position_90cm = rq3__webgazer_position_90cm['%EventsWithFixations'].mean()
 
-positions = ["50cm", "70cm", "90cm"]
+positions = ["TC4 (50cm)", "TC5 (70cm)", "TC6 (90cm)"]
 percentages_tobii = [percentage_events_including_fixations_rq3_tobii_position_50cm,
                      percentage_events_including_fixations_rq3_tobii_position_70cm,
                      percentage_events_including_fixations_rq3_tobii_position_90cm]
@@ -178,9 +179,9 @@ bars_tobii = ax.bar(x + width/2, percentages_tobii, width, label="Infrared/Tobii
 bars_webgazer = ax.bar(x - width/2, percentages_webgazer, width, label="Webcam/Webgazer.js", color='skyblue')
 
 # Añadir etiquetas y título
-ax.set_xlabel("Position")
-ax.set_ylabel("% Event Including Captured Fixations")
-ax.set_title("Comparison of Tools by Distance")
+ax.set_xlabel("Test Case (Distance from subject to screen)")
+ax.set_ylabel("% EIF")
+ax.set_title("% Events Including Fixation (%EIF) by device and distance")
 ax.set_xticks(x)
 ax.set_xticklabels(positions)
 ax.legend()
@@ -197,7 +198,7 @@ add_labels(bars_webgazer)
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ3_events_captured_fixations_bars.jpg')
+plt.savefig('output/figs/RQ3_events_captured_fixations_bars.jpg')
 # Mostrar gráfico
 # plt.show()
 
@@ -211,7 +212,7 @@ percentage_events_including_fixations_rq3_webgazer_position_50cm = rq3__webgazer
 percentage_events_including_fixations_rq3_webgazer_position_70cm = rq3__webgazer_position_70cm['%EventsWithFixations'].mean()
 percentage_events_including_fixations_rq3_webgazer_position_90cm = rq3__webgazer_position_90cm['%EventsWithFixations'].mean()
 
-positions = ["50cm", "70cm", "90cm"]
+positions = ["TC4 (50cm)", "TC5 (70cm)", "TC6 (90cm)"]
 percentages_tobii = [percentage_events_including_fixations_rq3_tobii_position_50cm,
                      percentage_events_including_fixations_rq3_tobii_position_70cm,
                      percentage_events_including_fixations_rq3_tobii_position_90cm]
@@ -226,16 +227,16 @@ plt.plot(positions, percentages_tobii, marker='o', label="Infrared/Tobii", color
 plt.plot(positions, percentages_webgazer, marker='o', label="Webcam/Webgazer.js", color='skyblue')
 
 # Añadir etiquetas, título y leyenda
-plt.xlabel("Position")
-plt.ylabel("% Event Including Captured Fixations")
-plt.title("Comparison of Tools by Distance")
+plt.xlabel("Test Case (Distance from subject to screen)")
+plt.ylabel("% EIF")
+plt.title("% Events Including Fixation (%EIF) by device and distance")
 plt.ylim(0, 110)  # Limitar el eje Y al rango de 0 a 110
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.legend()
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ3_events_captured_fixations_line.jpg')
+plt.savefig('output/figs/RQ3_events_captured_fixations_line.jpg')
 # Mostrar la gráfica 
 # plt.show()
 
@@ -249,7 +250,7 @@ percentage_matching_fixation_rq3_webgazer_position_70cm = rq3__webgazer_position
 percentage_matching_fixation_rq3_webgazer_position_90cm = rq3__webgazer_position_90cm['%MatchingFixations'].mean()
 
 tools = ["Infrared/Tobii","Webcam/Webgazer.js"]
-positions = ["50cm", "70cm", "90cm"]
+positions = ["TC4 (50cm)", "TC5 (70cm)", "TC6 (90cm)"]
 percentages_tobii = [percentage_matching_fixation_rq3_tobii_position_50cm,
                      percentage_matching_fixation_rq3_tobii_position_70cm,
                      percentage_matching_fixation_rq3_tobii_position_90cm]
@@ -267,8 +268,8 @@ bars_webgazer = ax.bar(x + width/2, percentages_webgazer, width, label="Webcam/W
 
 # Añadir etiquetas y título
 ax.set_xlabel("Position")
-ax.set_ylabel("% Matching Fixation")
-ax.set_title("Matching Fixation by Tool and Distance")
+ax.set_ylabel("% MF")
+ax.set_title("% Matching Fixation (%MF) by device and distance")
 ax.set_xticks(x)
 ax.set_xticklabels(positions)
 ax.set_ylim(0, 100)
@@ -280,13 +281,13 @@ add_labels(bars_webgazer)
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ3_matching_fixations_bars.jpg')
+plt.savefig('output/figs/RQ3_matching_fixations_bars.jpg')
 # Mostrar gráfico
 # plt.show()
 
 
 # Datos Matching fixations linea
-positions = ["50cm", "70cm", "90cm"]
+positions = ["TC4 (50cm)", "TC5 (70cm)", "TC6 (90cm)"]
 percentages_tobii = [percentage_matching_fixation_rq3_tobii_position_50cm,
                      percentage_matching_fixation_rq3_tobii_position_70cm,
                      percentage_matching_fixation_rq3_tobii_position_90cm]
@@ -301,15 +302,15 @@ plt.plot(positions, percentages_webgazer, marker='o', label="Webcam/Webgazer.js"
 
 # Añadir etiquetas, título y leyenda
 plt.xlabel("Position")
-plt.ylabel("% Matching Fixation")
-plt.title("Matching Fixation by Tool and Distance")
+plt.ylabel("% MF")
+plt.title("% Matching Fixations (%MF) by device and distance")
 plt.ylim(0, 100)  # Limitar el eje Y al rango de 0 a 100
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.legend()
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ3_matching_fixations_line.jpg')
+plt.savefig('output/figs/RQ3_matching_fixations_line.jpg')
 # Mostrar la gráfica
 # plt.show()
 
@@ -332,8 +333,8 @@ fig, ax = plt.subplots(figsize=(6, 4))
 bars = ax.bar(tools, percentages, color=[ 'orange','skyblue',])
 
 # Añadir etiquetas y título
-ax.set_ylabel("% Event Including Test Object Fixations")
-ax.set_title("Comparison of Tools: %Events  Test Object Fixations")
+ax.set_ylabel("% EITOF")
+ax.set_title("%Events Including Test Object Fixations (%EITOF) by device")
 ax.set_ylim(0, 110)  # Limitar el eje Y para espacio adicional
 
 # Mostrar porcentaje en las barras
@@ -344,7 +345,7 @@ for bar in bars:
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ4_event_including_test_object_fixations.jpg')
+plt.savefig('output/figs/RQ4_event_including_test_object_fixations.jpg')
 # Mostrar gráfico
 # plt.show()
 
@@ -362,8 +363,8 @@ fig, ax = plt.subplots(figsize=(6, 4))
 bars = ax.bar(tools, percentages, color=[ 'orange','skyblue',])
 
 # Añadir etiquetas y título
-ax.set_ylabel("% Matching Test Object Fixations")
-ax.set_title("Comparison of Tools: Matching Test Object Fixations")
+ax.set_ylabel("% MTOF")
+ax.set_title("% Matching Test Object Fixations (%MTOF) by device")
 ax.set_ylim(0, 110)  # Limitar el eje Y para espacio adicional
 
 # Mostrar porcentaje en las barras
@@ -374,9 +375,95 @@ for bar in bars:
 
 # Guardar la gráfica
 plt.tight_layout()
-plt.savefig('tests/figs/RQ4_matching_test_object_fixations.jpg')
+plt.savefig('output/figs/RQ4_matching_test_object_fixations.jpg')
 # Mostrar gráfico
 # plt.show()
 
 
-print("Plots correctly generated in folder: tests/figs")
+print("Plots correctly generated in folder: output/figs")
+
+
+
+
+# Crear un DataFrame para almacenar todos los resultados
+final_results = []
+
+# RQ1 Matching Fixation por Device y Test Case Scenario
+rq1_data = [
+    ['Infrared/Tobii (TC1)', percentage_matching_fixation_rq1_tobii_form_density_low],
+    ['Infrared/Tobii (TC2)', percentage_matching_fixation_rq1_tobii_form_density_high],
+    ['Webcam/Webgazer.js (TC1)', percentage_matching_fixation_rq1_webgazer_form_density_low],
+    ['Webcam/Webgazer.js (TC2)', percentage_matching_fixation_rq1_webgazer_form_density_high]
+]
+df_rq1 = pd.DataFrame(rq1_data, columns=['Device/Software (TC Scenario)', '% Matching Fixation'])
+final_results.append(df_rq1)
+
+# RQ2 Matching Fixation True por Tool/Software
+rq2_data = [
+    ['Infrared/Tobii', percentage_matching_fixation_rq2_tobii_alternance_buttons],
+    ['Webcam/Webgazer.js', percentage_matching_fixation_rq2_webgazer_alternance_buttons]
+]
+df_rq2 = pd.DataFrame(rq2_data, columns=['Device/Software', '% Matching Fixation'])
+final_results.append(df_rq2)
+
+# RQ2 Events Including Fixations por Tool/Software
+rq2_eif_data = [
+    ['Infrared/Tobii', percentage_events_including_fixations_rq2_tobii_alternance_buttons],
+    ['Webcam/Webgazer.js', percetange_events_including_fixations_rq2_webgazer_alternance_buttons]
+]
+df_rq2_eif = pd.DataFrame(rq2_eif_data, columns=['Device/Software', '% EIF'])
+final_results.append(df_rq2_eif)
+
+# RQ3 Events Including Fixations por Device y Distance
+rq3_eif_data = [
+    ['Infrared/Tobii (50cm)', percentage_events_including_fixations_rq3_tobii_position_50cm],
+    ['Infrared/Tobii (70cm)', percentage_events_including_fixations_rq3_tobii_position_70cm],
+    ['Infrared/Tobii (90cm)', percentage_events_including_fixations_rq3_tobii_position_90cm],
+    ['Webcam/Webgazer.js (50cm)', percentage_events_including_fixations_rq3_webgazer_position_50cm],
+    ['Webcam/Webgazer.js (70cm)', percentage_events_including_fixations_rq3_webgazer_position_70cm],
+    ['Webcam/Webgazer.js (90cm)', percentage_events_including_fixations_rq3_webgazer_position_90cm]
+]
+df_rq3_eif = pd.DataFrame(rq3_eif_data, columns=['Device/Software (Distance)', '% EIF'])
+final_results.append(df_rq3_eif)
+
+# RQ3 Matching Fixation por Device y Distance
+rq3_mf_data = [
+    ['Infrared/Tobii (50cm)', percentage_matching_fixation_rq3_tobii_position_50cm],
+    ['Infrared/Tobii (70cm)', percentage_matching_fixation_rq3_tobii_position_70cm],
+    ['Infrared/Tobii (90cm)', percentage_matching_fixation_rq3_tobii_position_90cm],
+    ['Webcam/Webgazer.js (50cm)', percentage_matching_fixation_rq3_webgazer_position_50cm],
+    ['Webcam/Webgazer.js (70cm)', percentage_matching_fixation_rq3_webgazer_position_70cm],
+    ['Webcam/Webgazer.js (90cm)', percentage_matching_fixation_rq3_webgazer_position_90cm]
+]
+df_rq3_mf = pd.DataFrame(rq3_mf_data, columns=['Device/Software (Distance)', '% MF'])
+final_results.append(df_rq3_mf)
+
+# RQ4 Events Including Test Object Fixations por Device
+rq4_eitof_data = [
+    ['Infrared/Tobii', percentage_events_including_test_object_fixations_rq4_tobii_rpm],
+    ['Webcam/Webgazer.js', percentage_events_including_test_object_fixations_rq4_webgazer_rpm]
+]
+df_rq4_eitof = pd.DataFrame(rq4_eitof_data, columns=['Device/Software', '% EITOF'])
+final_results.append(df_rq4_eitof)
+
+# RQ4 Matching Test Object Fixations por Device
+rq4_mtof_data = [
+    ['Infrared/Tobii', percentage_matching_test_object_fixations_rq4_tobii_rpm],
+    ['Webcam/Webgazer.js', percentage_matching_test_object_fixations_rq4_webgazer_rpm]
+]
+df_rq4_mtof = pd.DataFrame(rq4_mtof_data, columns=['Device/Software', '% MTOF'])
+final_results.append(df_rq4_mtof)
+
+# Combinar todos los DataFrames en uno solo
+final_df = pd.concat(final_results, ignore_index=True)
+
+# Obtener el timestamp actual
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Crear el nombre del archivo con el timestamp
+output_csv_path = f'output/final_results_exp_{timestamp}.csv'
+
+# Guardar el DataFrame en el archivo CSV con el nombre generado
+final_df.to_csv(output_csv_path, index=False)
+
+print(f"Datos guardados correctamente en: {output_csv_path}")
